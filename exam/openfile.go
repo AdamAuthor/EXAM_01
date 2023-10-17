e74cd569ffe1debb89df311f94f9ef48567949d7
aolzhass <aolzhass@L01R06S17s-iMac.local>
2023-10-18 02:12:47 +0600

    add all tasks

--- exam/openfile.go
+++ exam/openfile.go
@@ -23,7 +23,14 @@ func OpenFile(idxCheck, level int, task string) (string, string, string) {
 	var randomTask string
 	tasks := make(map[int][]string)
 	tasks[1] = []string{"countdown", "strlen"}
-	tasks[2] = []string{"firstrune", "lastrune", "rot13"}
+	tasks[2] = []string{"firstrune", "lastrune", "lastword", "reduceint", "rot13"}
+	tasks[3] = []string{"alphamirror", "chunk", "tabmult"}
+	tasks[4] = []string{"inter", "ispowerof2", "piglatin", "romannumbers", "swapbits", "union"}
+	tasks[5] = []string{"printhex", "repeatalpha"}
+	tasks[6] = []string{"hiddenp", "revwstr", "rostring", "slice", "split"}
+	tasks[7] = []string{"addprimesum", "atoibase", "fprime", "itoa"}
+	tasks[8] = []string{"brackets", "gcd", "grouping", "listsize", "options", "printmemory", "rpncalc"}
+	tasks[9] = []string{"brainfuck", "itoabase", "listremoveif"}
 
 	switch idxCheck {
 	case 1:
