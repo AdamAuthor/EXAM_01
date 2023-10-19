838421f746a39eeb296f360392a28042b5fa541c
aabdikha <aabdikha@L01R06S16s-iMac.local>
2023-10-19 21:13:36 +0600

    sordwordarr added

---
+++ .subjects/final-checkpoint/5/sortwordarr/solutions/sortwordarr.go
@@ -0,0 +1,11 @@
+package solutions
+
+func SortWordArr(a []string) {
+	for i := 0; i < len(a)-1; i++ {
+		for j := 0; j < len(a)-i-1; j++ {
+			if a[j] > a[j+1] {
+				a[j], a[j+1] = a[j+1], a[j]
+			}
+		}
+	}
+}
