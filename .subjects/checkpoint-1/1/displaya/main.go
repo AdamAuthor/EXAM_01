cc0e7f3497bf22b3a87d0cfc827854a54968e611
Aibekabdi <pigeonthe@gmail.com>
2023-11-26 00:35:45 +0600

    first tasks of 1 checpoint

---
+++ .subjects/checkpoint-1/1/displaya/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"academie/tester"
+	"strings"
+
+	"github.com/01-edu/go-tests/lib/chars"
+	"github.com/01-edu/go-tests/lib/random"
+)
+
+func main() {
+	table := append(random.StrSlice(chars.Words),
+		"dsfda",
+		"",
+		"1",
+		"1",
+	)
+	for _, s := range table {
+		tester.Program(".subjects/checkpoint-1/1/displaya/solutions", "displaya", strings.Fields(s)...)
+	}
+	tester.Program(".subjects/checkpoint-1/1/displaya/solutions", "displaya", "1", "a")
+}
